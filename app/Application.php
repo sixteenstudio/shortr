3f11d8dd49a8f6bfd1fe77fc241a953b2438f9a9
Matthew Collison <matthew@sixteenstudio.com>
2017-07-09 22:03:54 +0100

    Add the base application infrastructure and entry point

---
+++ app/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+class Application
+{
+
+    public function run()
+    {
+        $this->outputHeaders();
+        $this->outputContent();
+    }
+
+    public function outputHeaders()
+    {
+        // TODO: Output headers
+    }
+
+    public function outputContent()
+    {
+        // TODO: Output content
+
+    }
+
+}
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Require the composer auto-loader
+require('../vendor/autoload.php');
+
+// Instantiate the application instance
+$application = new \App\Application();
+
+// Run the application
+$application->run();
