daef9853a51123a8a077e0029273fa337fe78499
Matthew Collison <matthew@sixteenstudio.com>
2017-07-10 02:50:18 +0100

    Make not found view fit design

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,10 +1,9 @@
-<html>
-<head>
-    <title>Sorry, we couldn't find that page!</title>
-</head>
-<body>
+<?php include '../views/partials/head.php' ?>
+
+<div class="url-form">
     <h1>So sorry,</h1>
     <h2>The page you were looking for could not be found.</h2>
     <p><a href="/">Return Home</a></p>
-</body>
-</html>
+</div>
+
+<?php include '../views/partials/foot.php' ?>
